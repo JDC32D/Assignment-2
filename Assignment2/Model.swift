89d088ceabaab0b53533e218e3116b759b600be0
Carmody, Joseph (UMSL-Student) <jdc32d@es316m02.dhcp.umsl.edu>
2018-12-04 13:31:52 -0600

    Model Completed

--- Assignment2/Model.swift
+++ Assignment2/Model.swift
@@ -8,26 +8,68 @@
 
 import Foundation
 
+enum Filter: CaseIterable, CustomStringConvertible {
+    case noRings
+    case oneRing
+    case fiveRings
+    case sevenRings
+    case thirteenRings
+    case noMoons
+    case twoMoons
+    case fiveMoons
+    case fourteenMoons
+    case sixtyMoons
+    case sadPlanet
+    case kingOfPlanets
+    
+    var details: String {
+        switch self {
+            case .noRings: return "0 rings"
+            case .oneRing: return "1 ring"
+            case .fiveRings: return "5 rings"
+            case .sevenRings: return "7 rings"
+            case .thirteenRings: return "13 rings"
+            case .noMoons: return "0 moons"
+            case .twoMoons: return "2 moons"
+            case .fiveMoons: return "5 moons"
+            case .fourteenMoons: return "14 moons"
+            case .sixtyMoons: return "over 60 moons"
+            case .sadPlanet: return "Demoted Boi"
+            case .kingOfPlanets: return "King of Planets"
+        }
+    }
+}
+
 struct Planet {
     var name: String
-    var numMoons: String
+    var numMoons: Filter
     var moons = [String]()
     var position: String
-    var rings: String
+    var rings: Filter
+    
+    init(name: String, numMoons: Filter, moons: [String], position: String, rings: Filter) {
+        self.name = name
+        self.numMoons = numMoons
+        self.moons = moons
+        self.position = position
+        self.rings = rings
+    }
 }
 
 class PlanetModel {
     
     
     var Planets: [Planet] = [
-        Planet(name: "Mercury", numMoons: "0", moons: ["No Moons"], position: "1", rings: "0"),
-        Planet(name: "Venus", numMoons: "0", moons: ["No Moons"], position: "2", rings: "0"),
-        Planet(name: "Earth", numMoons: "0", moons: ["The Moon!"], position: "3", rings: "0"),
-        Planet(name: "Mars", numMoons: "2", moons: ["Phobos, Deimos"], position: "4", rings: "0"),
-        Planet(name: "Jupiter", numMoons: "67", moons: ["Europa", "Ganymede", "Io", "Callisto"], position: "5", rings: "0"),
-        Planet(name: "Saturn", numMoons: "62", moons: ["Titan","Enceladus","Mimas","Iapetus","Dione","Rhea","Tethys","Phoebe","Hyperion","Pandora","Prometheus", "Janus","Pan"], position: "6", rings: "7"),
-        Planet(name: "Uranus", numMoons: "5", moons: ["Umbriel","Titania","Miranda","Oberon","Ariel","Trinculo","Cordelia","Francisco", "Setebos", "Rosalind"], position: "5", rings: "13"),
-        Planet(name: "Neptune", numMoons: "14", moons: ["Naiad", "Thalassa", "Despina", "Galatea", "Larissa"], position: "5", rings: "5")
+        Planet(name: "Mercury", numMoons: Filter.noMoons, moons: ["No Moons"], position: "1", rings: Filter.noRings),
+        Planet(name: "Venus", numMoons: Filter.noMoons, moons: ["No Moons"], position: "2", rings: Filter.noRings),
+        Planet(name: "Earth", numMoons: Filter.noMoons, moons: ["The Moon!"], position: "3", rings: Filter.noRings),
+        Planet(name: "Mars", numMoons: Filter.twoMoons, moons: ["Phobos, Deimos"], position: "4", rings: Filter.noRings),
+        Planet(name: "Jupiter", numMoons: Filter.sixtyMoons, moons: ["Europa", "Ganymede", "Io", "Callisto"], position: "5", rings: Filter.noRings),
+        Planet(name: "Saturn", numMoons: Filter.sixtyMoons, moons: ["Titan","Enceladus","Mimas","Iapetus","Dione","Rhea","Tethys","Phoebe","Hyperion","Pandora","Prometheus", "Janus","Pan"], position: "6", rings: Filter.sevenRings),
+        Planet(name: "Uranus", numMoons: Filter.fiveMoons, moons: ["Umbriel","Titania","Miranda","Oberon","Ariel","Trinculo","Cordelia","Francisco", "Setebos", "Rosalind"], position: "7", rings: Filter.thirteenRings),
+        Planet(name: "Neptune", numMoons: Filter.fourteenMoons, moons: ["Naiad", "Thalassa", "Despina", "Galatea", "Larissa"], position: "8", rings: Filter.fiveRings),
+        Planet(name: "Sun", numMoons: Filter.kingOfPlanets, moons: ["No Moons"], position: "0", rings: Filter.noRings),
+        Planet(name: "Pluto" , numMoons: Filter.fiveMoons, moons: ["Charon", "Styx", "Nix", "Kerberos", "Hydra"], position: "9", rings: Filter.sadPlanet )
     ]
     
 }
